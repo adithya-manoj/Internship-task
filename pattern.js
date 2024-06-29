071e3a261a426e22634fe5db2cb432cd368d91be
Adithya Manoj <84499430+adithya-manoj@users.noreply.github.com>
2024-06-29 15:35:40 +0530

    Update pattern.js

--- pattern.js
+++ pattern.js
@@ -1,16 +1,10 @@
 function createPattern(rows, cols) {
     cols=parseInt(cols/2+1)
-    console.log(cols);
     let pattern = '';
 pattern += ' ___    '.repeat(cols) + '\n';
-
-
 pattern += '/   \\___'.repeat(cols-1) ;
-pattern+='/   \\'+ '\n'
-  
-    
+pattern+='/   \\'+ '\n'    
     for (let row = 0; row < rows; row++) {
-    
         for (let col = 0; col < cols; col++) {
             pattern += '\\___/';
             if (col < cols - 1) {
@@ -20,7 +14,6 @@ pattern+='/   \\'+ '\n'
                 pattern+='   \\'
             }
         }
-        
         if (row < rows - 1) {
             pattern += '\n';
             for (let col = 0; col < cols-1; col++) {
@@ -28,20 +21,14 @@ pattern+='/   \\'+ '\n'
                 if(col===cols-2){
                     pattern += '/';
                     pattern+='   \\\n'
-                }
-                
+                }    
             }
-            
-            
-            // pattern += '/\n';
         } else {
             pattern += '\n';
         }
     }
-
     return pattern;
 }
-
 const rows = 4; // Number of rows
 const cols =7; // Number of columns
 console.log(createPattern(rows, cols));
